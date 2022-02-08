8fcb39e6c5174833932ba6e32e6c2125c2330799
Herberth Orlando Mancía Cisneros <her_m.c@hotmail.com>
2022-02-08 10:58:43 -0600

    operaciones

--- src/Administrator/PTN_BOM.js
+++ src/Administrator/PTN_BOM.js
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 import React, { useEffect } from 'react'
 import  {useState} from 'react';
 import './css/PTN_BOM.css';
-import {total} from '../Components/Operaciones'
+import {operaciones} from '../Components/Operaciones'
 import Table from 'react-bootstrap/Table'
 
 
@@ -26,13 +25,18 @@ total                        DOUBLE
 descuento                    DOUBLE
 moneda {value  = 2 opciones} VARCHAR
  */
+const { total, precio_u}= operaciones();
+const {punitario , setpunitario} = useState({
+  total: "2"
+  
+});
 const [datos,setDatos] = useState ({
   clave:"",
   descripcion_proyecto: "",
   cliente: "",
   valor_dolar: "",
-  Partida:"",
-  precio_lista:"",
+  Partida: "",
+  precio_lista :"",
   precio_unitario:"",
   descuento:"",
   cantidad:""
@@ -51,13 +55,12 @@ const enviarDatos = (event) => {
 }
 
 useEffect(() =>{
-  
-  console.log (datos.precio_unitario)
-  console.log (datos.cantidad)
-  console.log (datos.descuento)
- total(datos.precio_unitario,datos.cantidad,datos.descuento);
- 
-});
+  console.log("total:  ");
+  const hola =total(datos.precio_lista,datos.cantidad,datos.descuento);
+  console.log(hola);
+  console.log("Precio unitario :");
+  console.log(precio_u(hola,datos.cantidad));
+  },[datos.cantidad, datos.descuento, datos.precio_lista, precio_u, total]);
    
 
 
@@ -157,352 +160,6 @@ useEffect(() =>{
                 <th>Descripción</th>
                 <th> Duración Meses </th>
                 <th> Entrega </th>
-                <th> Cantidad </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="">
-                <td>
-                  <input className="agregar"
-                    type="number"
-                    name="no_parte"
-                    onChange={handleInputChange}
-                    placeholder="No. Parte" />
-                </td>
-
-                <td>  <input className="agregar"
-                  type="text"
-                  name="descripcion"
-                  onChange={handleInputChange}
-                  placeholder="Descripción" />
-                </td>
-
-                <td> <input className="agregar"
-                  type="number"
-                  name="meses"
-                  onChange={handleInputChange}
-                  min="0"
-                  placeholder="Meses" />
-                </td>
-
-                <td>
-                  <input className="agregar"
-                    type="number"
-                    name="entrega_semanas"
-                    onChange={handleInputChange}
-                    min="0"
-                    placeholder="Entrega semanas" />
-                </td>
-                
-                <td> <input className="agregar"
-                  type="number"
-                  name="cantidad"
-                  onChange={handleInputChange}
-                  min="0"
-                  placeholder="Cantidad" />
-                </td>
-              </tr>
-
-            </tbody>
-          </Table>
-
-
-
-          <Table responsive id="nombreDiv">
-            <thead>
-              <tr className="titulo-tabla-usuarios">
-                <th>Precio Lista</th>
-                <th>Precio Unitario</th>
-                <th>Descuento (%) </th>
-                <th> Moneda</th>
-                <th> Total </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="">
-                <td>  <input className="agregar"
-                  type="number"
-                  name="precio_lista"
-                  onChange={handleInputChange}
-                  placeholder="Precio Lista" 
-                  min="0"
-                  step="any"             
-                  />
-                </td>
-                <td>   <input className="agregar"
-                  type="number"
-                  name="precio_unitario"
-                  //onClick={}
-                  onChange={handleInputChange}
-                  placeholder="Precio Unitario"
-                  min="0"
-                  step="any"
-                   />
-                </td>
-                <td> <input className="agregar"
-                  type="number"
-                  name="total"
-                  onChange={handleInputChange}
-                  placeholder="Total" 
-                  min="0"
-                  step="any"
-                  
-                  />
-                </td>
-
-                <td> <input className="agregar"
-                  type="number"
-                  name="descuento"
-                  onChange={handleInputChange}
-                  placeholder="Descuento" 
-                  min="0"
-                  step="any"
-                  />
-                  
-                
-                </td>
-                <td>
-                  <select id="moneda" name="moneda" onChange={handleInputChange}>
-                    <option value="lista 1">MXN</option>
-                    <option value="lista 2">USD</option>
-                  </select>
-                </td>
-
-
-                <td>   <input className="agregar"
-                  type="number"
-                  name="total"
-                  placeholder="Total" 
-                  min="0"
-                  step="any"
-                  
-                  />
-                </td>
-              
-              </tr>
-
-            </tbody>
-          </Table>
-
-
-
-          <Table responsive id="nombreDiv">
-            <thead>
-              <tr className="titulo-tabla-usuarios">
-                <th>Marca</th>
-                <th>Proveedor</th>
-                <th>Comentarios </th>
-                <th>Categoria </th>
-                <th> - </th>
-
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="">
-                <td>  <input className="agregar"
-                  type="text"
-                  name="marca"
-                  onChange={handleInputChange}
-                  placeholder="Marca" />
-                </td>
-                <td>  <input className="agregar"
-                  type="text"
-                  name="provedor"
-                  onChange={handleInputChange}
-                  placeholder="Proveedor" />
-                </td>
-                <td> <input className="agregar"
-                  type="text"
-                  name="comentarios"
-                  onChange={handleInputChange}
-                  placeholder="Comentarios" />
-                </td>
-
-                <td>     <select id="lista-opciones"  onChange={handleInputChange}>
-                  <option value="lista 1">Técnologia Principal</option>
-                  <option value="lista 2">Sub-tecnlogía</option>
-                  <option value="lista 3">Equipamiento</option>
-                  <option value="lista 1">Licencia</option>
-                  <option value="lista 2">Soporte</option>
-                  <option value="lista 3">Implementación</option>
-                  <option value="lista 1">Capacitación</option>
-                  <option value="lista 2">Accesorios</option>
-                  <option value="lista 3">Soporte PTN</option>
-                  <option value="lista 2">Implementación PTN</option>
-                  <option value="lista 3">Mesa de Ayuda PTN</option>
-
-                </select>
-                </td>
-
-
-                <td>
-                  <button className="btn btn-primary"> agregar</button>
-                </td>
-              </tr>
-
-            </tbody>
-          </Table>
-         
-
-
-        </div>
-
-        {/*  Añadir */}
-
-        {/* <div className="boton-ptn-agregar"  >
-          <button className="btn btn-primary"> + Añadir </button>
-        </div> */}
-
-        {/* Finalizar */}
-        <div className="boton-ptn-finalizar"  >
-          <button className="btn btn-danger" onClick={enviarDatos}> Finalizar </button>
-        </div>
-        </form>
-
-
-      </div>
-    </div>
-  )
-}
-
-=======
-import React from 'react'
-import  {useState} from 'react';
-import './css/PTN_BOM.css'
-
-import Table from 'react-bootstrap/Table'
-
-
-function PTN_BOM() {
-
-
-/* 
-names INPUT para PTN_BOM
-clave                        VARCHAR
-descripcion_proyecto         VARCHAR
-cliente {value = 3 opciones} VARCHAR
-valor_dolar                  DOUBLE
-no_parte                     INT
-descripcion                  VARCHAR
-meses                        INT
-entrega_semanas              INT
-cantidad                     INT
-precio_lista                 DOUBLE
-precio_unitario              DOUBLE
-total                        DOUBLE
-descuento                    DOUBLE
-moneda {value  = 2 opciones} VARCHAR
- */
-const [datos,setDatos] = useState ({
-  clave:"",
-  descripcion_proyecto: "",
-  cliente: "",
-  valor_dolar: "",
-  Partida:""
-
-});
-const handleInputChange = (event) => {
-  //console.log(event.target.value)
-  setDatos ({
-      ...datos,[event.target.name] : event.target.value ,
-  })
-};
-
-const enviarDatos = (event) => {  
-  console.log(datos.clave);
-  event.preventDefault();
-  
-}
-   
-
-
-  return (
-
-    <div className="contenido-usuarios">
-
-      <div className="table-responsive">
-
-        <h2>PTN BOM  Cotizaciones</h2>
-
-        {/*  Nuevo Proyecto*/}
-        <div className="boton-ptn"  >
-          <button className="btn btn-success"> + Nuevo Proyecto</button>
-        </div>
-        <form  >
-
-        <div className="opciones-proyecto">
-         
-          <Table responsive id="nombreDiv" striped bordered hover >
-            <thead>
-
-              <tr className="titulo-tabla-usuarios">
-                <th>Clave</th>
-                <th>Descripción</th>
-                <th> Cliente </th>
-              
-              </tr>
-
-            </thead>
-
-            <tbody>
-              <tr className="">
-                <td>
-                  <input className="agregar"
-                    type="text"
-                    name="clave"
-                    onChange={handleInputChange}
-                    placeholder="ingrese Clave" />
-
-                </td>
-                <td>
-                  <input className="agregar"
-                    type="text"
-                    name="descripcion_proyecto"
-                    onChange={handleInputChange}
-                    placeholder="ingrese Descripción" />
-                </td>
-                <td>     <select id="lista-proyectos" name="cliente" onChange={handleInputChange}>
-                  <option value="">Elige una opción</option>
-                  <option value="lista 1">Cliente 1</option>
-                  <option value="lista 2">Cliente 2</option>
-                  <option value="lista 3">Cliente 3</option>
-                </select>
-                </td>
-             
-              </tr>
-
-              <tr className="boton-agregar-proyecto">
-                <td></td>
-                <td></td>            
-                <td>   <button className="btn btn-primary"> Añadir </button>    </td>
-              </tr>
-
-            </tbody>
-          </Table>
-
-
-          <div className="partida">
-          <input className="agregar"
-                 type="text"
-                 name="Partida"
-                 onChange={handleInputChange}
-                 placeholder="ingrese Nombre Partida" />
-                 <br/>
-                 <br/>
-            <button className="btn btn-primary"> Partida </button>  
-                
-              <br/>
-              <br/>
-          </div>
-
-
-          <Table responsive id="nombreDiv">
-            <thead>
-              <tr className="titulo-tabla-usuarios">
-                <th>No. De Parte</th>
-                <th>Descripción</th>
-                <th> Duración Meses </th>
-                <th> Entrega </th>
                 <th> Moneda </th>
               </tr>
             </thead>
@@ -541,7 +198,7 @@ const enviarDatos = (event) => {
                 </td>
                 
                 <td> 
-                    <select id="moneda" name="moneda" onChange={handleInputChange}>
+                   <select id="moneda" name="moneda" onChange={handleInputChange}>
                     <option value="lista 1">MXN</option>
                     <option value="lista 2">USD</option>
                   </select>
@@ -558,8 +215,8 @@ const enviarDatos = (event) => {
               <tr className="titulo-tabla-usuarios">
                 <th>Cantidad</th>
                 <th>Precio Lista</th>
-                <th>Precio Unitario </th>
-                <th> Descuneto (%)</th>
+                <th>Precio Unitario</th>
+                <th> Descuento (%)</th>
                 <th> Total </th>
               </tr>
             </thead>
@@ -569,7 +226,7 @@ const enviarDatos = (event) => {
                   type="number"
                   name="cantidad"
                   onChange={handleInputChange}
-                  placeholder="Cantidad" 
+                  placeholder="Cantidad " 
                   min="0"
                   step="any"             
                   />
@@ -587,35 +244,33 @@ const enviarDatos = (event) => {
                   type="number"
                   name="precio_unitario"
                   onChange={handleInputChange}
-                  placeholder="Precio Unitario" 
+                  placeholder="Precio unitario" 
                   min="0"
                   step="any"
                   
                   />
                 </td>
+
+                <td> <input className="agregar"
+                  type="number"
+                  name="descuento"
+                  onChange={handleInputChange}
+                  placeholder="Descuento" 
+                  min="0"
+                  step="any"
+                  />              
+                </td>
+              
 
 
                 <td>   <input className="agregar"
-                  type="number"
-                  name="descuento"
-                  placeholder="Descuento %" 
+                  type="text"
+                  placeholder="Total" 
                   min="0"
                   step="any"
                   
                   />
                 </td>
-
-                <td>
-                <input className="agregar"
-                  type="number"
-                  name="total"
-                  onChange={handleInputChange}
-                  placeholder="Total" 
-                  min="0"
-                  step="any"             
-                  />
-                </td>
-
               
               </tr>
 
@@ -703,5 +358,4 @@ const enviarDatos = (event) => {
   )
 }
 
->>>>>>> 80d8e5c29496664027b27f2c7034f1b3d0a6747f
 export default PTN_BOM
--- src/Components/Operaciones.js
+++ src/Components/Operaciones.js
@@ -1,37 +1,58 @@
-const total = (p_unitario,cantidad) => {
-
-    if (p_unitario === "" && cantidad === ""){
-        p_unitario = 0;
-        cantidad = 0;
-        return console.log((Number.parseFloat(p_unitario) * Number.parseFloat(cantidad)));
-
-    }
-     if (p_unitario !== "" && cantidad === ""){
-        cantidad = 0;
-        return console.log((Number.parseFloat(p_unitario) * Number.parseFloat(cantidad)));
-    }
-
-    else if(cantidad !== '' && p_unitario === ""){
-        p_unitario=0;
-        return console.log((Number.parseFloat(p_unitario) * Number.parseFloat(cantidad)));
-
-        }
-        else {
-            return console.log((Number.parseFloat(p_unitario) * Number.parseFloat(cantidad)));
-        }
-
+export const operaciones = ()=>{
+    const total = (p_lista,cantidad,descuento) => {   
+        if (p_lista === "" && cantidad === "" && descuento === ""){
+            p_lista = 0;
+            cantidad = 0;
+            descuento = 0;
+            const resultado = (Number.parseFloat(p_lista) * Number.parseFloat(cantidad))
+            return resultado*(1-(Number.parseFloat(descuento)/100));
     
-     
-       
+        }
+        else if (p_lista !== "" && cantidad === "" && descuento === ""){
+            descuento = 0;
+            cantidad = 0;
+            const resultado = (Number.parseFloat(p_lista) * Number.parseFloat(cantidad))
+            return resultado*(1-(Number.parseFloat(descuento)/100));
+        }
+    
+        else if(cantidad !== '' && p_lista === "" && descuento === "" ){
+            p_lista=0;
+            descuento= 0;
+            const resultado = (Number.parseFloat(p_lista) * Number.parseFloat(cantidad))
+            return resultado*(1-(Number.parseFloat(descuento)/100));
+            }
+    
+        else if(descuento !== "" && p_lista === "" && cantidad === ""){
+           cantidad=0;
+           p_lista= 0;
+            const resultado = (Number.parseFloat(p_lista) * Number.parseFloat(cantidad))
+            return resultado*(1-(Number.parseFloat(descuento)/100));
+            }
+        else if(cantidad !== '' && p_lista !== "" && descuento === "" ){
+            descuento= 0;
+            const resultado = (Number.parseFloat(p_lista) * Number.parseFloat(cantidad))
+            return resultado*(1-(Number.parseFloat(descuento)/100));
+        }    
+            
+            else {
+                const resultado = (Number.parseFloat(p_lista) * Number.parseFloat(cantidad))
+                return resultado*(1-(Number.parseFloat(descuento)/100));
+            }
+    
+        
+         
+           
+        };
+    const precio_u = (total, cantidad)=> Number.parseFloat(total)/Number.parseFloat(cantidad) ;
+    const descuento_1= (precio_u,precio_l)=>Number.parseFloat(precio_u)/Number.parseFloat(precio_l);
+    
+    return {
+        total,
+        precio_u,
+        descuento_1        
     };
     
-   
-    
-    
 
-const precio_unitario = (p_lista,descuento) => {return (p_lista(1-descuento))};
-const descuento =(p_unitario,p_lista)=> (1-(p_unitario/p_lista))*100;
+}
 
 
-    
-export {total , precio_unitario , descuento}
