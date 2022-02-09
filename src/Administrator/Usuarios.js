b2763c96c94b9c0990da8ada14c2a4d741883f10
Herberth Orlando Mancía Cisneros <her_m.c@hotmail.com>
2022-02-08 19:44:54 -0600

    Boton de eleminar registro esta en funcionamiento

--- src/Administrator/Usuarios.js
+++ src/Administrator/Usuarios.js
@@ -1,22 +1,43 @@
-import React from "react";
+import React ,{useState} from "react";
 import "./css/Usuarios.css";
 import Table from 'react-bootstrap/Table'
+import axios from 'axios';
 
-
-
-const listaUsuarios = [
+/*const listaUsuarios = [
     {id: 1, rol: 'Rol', correo: 'Correo' , contrasena: 'Contraseña'},
     {id: 2, rol: 'Administrador', correo: 'oscar@delfos369.com' , contrasena: '12345'},
     {id: 3, rol: 'Preventa', correo: 'usuario@delfos369.com' , contrasena: '12345'}
-  ];
-
+  ];*/
 
 function Usuarios(prop) {
+
+    const [listaUsuarios,setlistaUsarios ] = useState([]);
+    const borrarUsuario = async(dato)=>{
+        const confirmacion =window.confirm("¿Seguro que quieres borrar este registro?");
+        if(confirmacion){
+          console.log(dato);
+          const respuesta = await axios.get(`http://localhost:4001/api/cotizador/delete/${dato}`);
+          console.log(respuesta.data);
+          llamadoUsuario();
+        }else{
+            llamadoUsuario();
+        }
+    };
+
+    const llamadoUsuario = async() => {
+        const respuesta = await axios.get('http://localhost:4001/api/cotizador/registro');
+        setlistaUsarios (respuesta.data.reSql);
+    }
+    
+    
+    
     return (
+        
         <div className="contenido-usuarios">
                <div className="table-responsive">
                <div className="titulo-proyectos">
                 <h2>Lista de Usuarios </h2>
+                
             </div>
                 <Table responsive  striped bordered hover size="sm">
                     <thead>
@@ -25,23 +46,30 @@ function Usuarios(prop) {
                                        
          <tbody>
       {Object.keys(listaUsuarios).map((key) => (
-
-          //checar aqui va los titulos
-        <tr key={listaUsuarios[key].id} >    
-
-        
-        <td>{listaUsuarios[key].rol}</td>
-        <td>{listaUsuarios[key].correo}</td>
-        <td>{listaUsuarios[key].contrasena}</td>
-        </tr>
      
+          //checar aqui va los titulos
+        <tr key={listaUsuarios[key].id_usuario} >
+            <td>{listaUsuarios[key].id_usuario}</td>
+            <td>{listaUsuarios[key].rol}</td>
+            <td>{listaUsuarios[key].email}</td>
+            <td>{listaUsuarios[key].password}</td>
+            <td><button  onClick={() =>borrarUsuario(listaUsuarios[key].id_usuario)}> borrar</button></td>
+       
+        </tr>
+       
        ))
       }
-    </tbody>
-                        
+     
+     </tbody>
+       
+            
                 </Table>
             </div>
+            <div><button onClick={llamadoUsuario}>actulizar</button></div>
+          
+         
         </div>
+        
     );
 }
 
