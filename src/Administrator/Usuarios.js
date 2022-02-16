0eaa7d7adb7aa67f0a4579a61fd54f954608a997
Herberth Orlando Mancía Cisneros <her_m.c@hotmail.com>
2022-02-16 16:50:00 -0600

    error arreglado den administracion de usuarios

--- src/Administrator/Usuarios.js
+++ src/Administrator/Usuarios.js
@@ -17,52 +17,63 @@ function Usuarios(prop) {
         } = useRegistro();
 
     const [listaUsuarios,setlistaUsarios ] = useState([]);
-    const [activado,setactivado] = useState(true);
-    const [validar , setvalidar] = useState({})
-    const book ={}
-
-
-    const enable =(key)=>
-    {
-        setvalidar({
-            [key] : activado,            
-        })
-
-        console.log(validar);
-    }
-        
-    
-     const borrarUsuario = async(dato)=>{
+    const [validar , setvalidar] = useState([]);
+    const borrarUsuario = async(dato)=>{
         const confirmacion =window.confirm("¿Seguro que quieres borrar este registro?");
         if(confirmacion){
           console.log(dato);
           const respuesta = await axios.get(`http://localhost:4001/api/cotizador/delete/${dato}`);
           console.log(respuesta.data);
-          llamadoUsuario();
+          llamado();
         }else{
-            llamadoUsuario();
+            llamado();
         }
     };
+    
+    const enable=(key)=>{
+       const newARR =[]
+       console.log(validar);
+       let i =  Object.keys(listaUsuarios);
+       for(let x = 0 ; x < i.length ; x++){
+           
+                newARR[x] = validar[0][x];
+            }
+            console.log(newARR);    
+        for(let y =0 ; y <i.length;y++){
+            if(y == key){
+                newARR[y]=!validar[0][y];
 
-     const llamadoUsuario = async() => {
+            }
+        }   
+
+        setvalidar([newARR])
+
+        console.log(newARR)
+            
+    }
+      
+    const llamado = async()=>{
         const respuesta = await axios.get('http://localhost:4001/api/cotizador/registro');
-        const i = Object.keys(respuesta.data.reSql);
-        for(let j=0 ;j < i.length;j++ ){
-            
-            setvalidar({
-                [j] : true,            
-            })
-            
-        }
-        console.log()
-
-       setlistaUsarios (respuesta.data.reSql);
-        
+        setlistaUsarios (respuesta.data.reSql);
         
     }
     
     
-    
+    const llamadoUsuario = async() => {
+        const newValidar =[];       
+        const respuesta = await axios.get('http://localhost:4001/api/cotizador/registro');
+        let i = Object.keys(respuesta.data.reSql);
+        for(let x =0 ; x < i.length ; x++){
+          
+            newValidar[x]=true;
+           
+        }
+        setvalidar([...validar , newValidar])
+        setlistaUsarios (respuesta.data.reSql);
+        
+      
+    }
+  
     return (
         
         <div className="contenido-usuarios">
@@ -71,15 +82,17 @@ function Usuarios(prop) {
                 <h2>Lista de Usuarios </h2>
                 
             </div>
-
-
             <div>
-                <button className= "btn btn-primary actualizar" onClick={llamadoUsuario}>Actualizar Datos</button>
+            <button    className= "btn btn-primary actualizar" onClick={llamadoUsuario}>Mostrart lista</button>
+            <br/>
+            <br/>
+            <button    className= "btn btn-primary actualizar" >Aceptar cambios</button>
            <br/>
            <br/>
            </div>
-          
+             
                 <Table responsive  striped bordered hover size="sm">
+                
                     <thead>
                     <tr className="titulo-tabla-usuarios">
                                 <th>ID</th>
@@ -92,17 +105,17 @@ function Usuarios(prop) {
                     </thead>
                                        
          <tbody>
-      {Object.keys(listaUsuarios).map((key) => (
-          
          
-          //checar aqui va los titulos
+
+           {Object.keys(listaUsuarios).map((key) => (
+           //checar aqui va los titulos
         <tr key={listaUsuarios[key].id_usuario} >
             <td>{listaUsuarios[key].id_usuario}</td>        
-            <td><input  className="input-name" defaultValue={listaUsuarios[key].rol} onChange={handleInputChange} disabled={validar[key]} name="rol"      id={listaUsuarios[key].id_usuario}></input></td>
-            <td><input  className="input-name" defaultValue={listaUsuarios[key].email} onChange={handleInputChange} disabled={validar[key]} name="email"></input> </td>                     
-            <td><input  className="input-name" defaultValue={listaUsuarios[key].password} onChange={handleInputChange} disabled={validar[key]} name="password"></input> </td> 
+            <td><input  className="input-name" defaultValue={listaUsuarios[key].rol} onChange={handleInputChange} disabled={validar[0][key]} name="rol"      id={listaUsuarios[key].id_usuario}></input></td>
+            <td><input  className="input-name" defaultValue={listaUsuarios[key].email} onChange={handleInputChange} disabled={validar[0][key]} name="email"></input> </td>                     
+            <td><input  className="input-name" defaultValue={listaUsuarios[key].password} onChange={handleInputChange} disabled={validar[0][key]} name="password"></input> </td> 
             <td><button className="btn btn-primary eliminar" onClick={()=>borrarUsuario(listaUsuarios[key].id_usuario)}> borrar</button></td>
-            <td><button className="btn btn-primary modificar" onClick={()=>enable(key)}>Actualizar</button></td> 
+            <td><button   className="btn btn-primary modificar" onClick={()=>enable(key)} >Actualizar</button></td> 
         </tr>  
        
        ))
@@ -113,8 +126,7 @@ function Usuarios(prop) {
             
                 </Table>
             </div>
-
-        
+              
          
         </div>
         
