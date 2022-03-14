7ac7bdd1d7234395c908e85da7c66be6affa8a13
CarlosDS369 <carlos@delfos369.com>
2022-03-14 09:01:47 -0600

    Update GuardarDatosPrecio.js

--- src/Routes/GuardarDatosPrecio.js
+++ src/Routes/GuardarDatosPrecio.js
@@ -1,48 +1,48 @@
-import  {useState} from 'react';
-import axios from 'axios';
+// import  {useState} from 'react';
+// import axios from 'axios';
 
-export const GuardarDatosPrecio = () => {
-    const [datos, setDatos] = useState ({
-        precio_lista: '',
-        precio_unitario: '',
-        precio_descuento: '',   
-    });
+// export const GuardarDatosPrecio = () => {
+//     const [datos, setDatos] = useState ({
+//         precio_lista: '',
+//         precio_unitario: '',
+//         precio_descuento: '',   
+//     });
 
-    const handleChange = (event) => {
-        setDatos({
-            ...datos,[event.target.name] : event.target.value ,
-        })
-    }
+//     const handleChange = (event) => {
+//         setDatos({
+//             ...datos,[event.target.name] : event.target.value ,
+//         })
+//     }
     
 
-    async function Send (){
-        const data = {
-            precio_lista: datos.precio_lista,
-            precio_unitario: datos.precio_unitario,
-            precio_descuento: datos.precio_descuento
-        };
-        console.log("este es el send de guardar datos precio", Send) 
+//     async function Send (){
+//         const data = {
+//             precio_lista: datos.precio_lista,
+//             precio_unitario: datos.precio_unitario,
+//             precio_descuento: datos.precio_descuento
+//         };
+//         console.log("este es el send de guardar datos precio", Send) 
 
-        try{
-            const respuesta = await axios.post('http://localhost:4001/api/cotizador/precio/agregar', data);
-            const send2 = respuesta.data;
+//         try{
+//             const respuesta = await axios.post('http://localhost:4001/api/cotizador/precio/agregar', data);
+//             const send2 = respuesta.data;
             
-            alert('Registro exitoso')
-            }
-            catch (error){
-            console.log("este es el error", error);
-            }
-    }
-    const enviarDatosPrecio = (event) =>{
-        console.log("estos son los datos", datos) 
-        Send();
+//             alert('Registro exitoso')
+//             }
+//             catch (error){
+//             console.log("este es el error", error);
+//             }
+//     }
+//     const enviarDatosPrecio = (event) =>{
+//         console.log("estos son los datos", datos) 
+//         Send();
         
-        // event.preventDefault()
-        // event.target.reset();
-    }
-    return{
-        handleChange,
-        enviarDatosPrecio
-    }
+//         // event.preventDefault()
+//         // event.target.reset();
+//     }
+//     return{
+//         handleChange,
+//         enviarDatosPrecio
+//     }
     
-}
+// }
