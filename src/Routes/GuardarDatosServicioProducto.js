5e3e6a072a5c0c7c38238b1460ce4b2fd0d4e52c
CarlosDS369 <carlos@delfos369.com>
2022-03-14 09:01:43 -0600

    Update GuardarDatosServicioProducto.js

--- src/Routes/GuardarDatosServicioProducto.js
+++ src/Routes/GuardarDatosServicioProducto.js
@@ -1,56 +1,54 @@
-import  {useState} from 'react';
-import axios from 'axios';
+// import  {useState} from 'react';
+// import axios from 'axios';
 
-export const GuardarDatosServicioProducto = () => {
-    const[datos, setDatos] = useState  (
-        {
-            sp_no_parte: '',
-            sp_descripcion: '',
-            sp_meses: '',
-            sp_semanas: '',
-            // sp_cantidad: '',
-            sp_comentarios: ''
-        });
+// export const GuardarDatosServicioProducto = () => {
+//     const[datosSP, setDatosSP] = useState  (
+//         {
+//             sp_no_parte: '',
+//             sp_descripcion: '',
+//             sp_meses: '',
+//             sp_semanas: '',
+//             sp_cantidad: '',
+//             sp_comentarios: ''
+//         });
 
-    const handleChange = (event) =>{
-        setDatos({
-            ...datos, [event.target.name] : event.target.value
-        })
-    }
+//     const handleChangeSP = (event) =>{
+//         setDatosSP({
+//             ...datosSP, [event.target.name] : event.target.value
+//         })
+//     }
     
-    async function Send (){
-        const data = {
-            sp_no_parte: datos.sp_no_parte,
-            sp_descripcion: datos.sp_descripcion,
-            sp_meses: datos.sp_meses,
-            sp_semanas: datos.sp_semanas,
-            sp_cantidad: datos.sp_cantidad,
-            sp_comentarios: datos.sp_comentarios
-        };
+//     async function SendSP (){
+//         const data = {
+//             sp_no_parte: datosSP.sp_no_parte,
+//             sp_descripcion: datosSP.sp_descripcion,
+//             sp_meses: datosSP.sp_meses,
+//             sp_semanas: datosSP.sp_semanas,
+//             sp_cantidad: datosSP.sp_cantidad,
+//             sp_comentarios: datosSP.sp_comentarios
+//         };
 
-        try{
-            console.log("hola, soy guardar data servicio producto", data)
+//         try{
+//             console.log("hola, soy guardar data servicio producto", data)
 
-            const respuesta = await axios.post('http://localhost:4001/api/cotizador/sp/agregar', data);
-            const send2 = respuesta.data;
-            console.log("hola soy send2", send2); 
-            alert('Registro exitoso')
-            }
-            catch (error){
-            console.log("este es el error", error);
-        }
+//             const respuesta = await axios.post('http://localhost:4001/api/cotizador/sp/agregar', data);
+//             const send2 = respuesta.data;
+//             console.log("hola soy send2", send2); 
+//             alert('Registro exitoso')
+//             }
+//             catch (error){
+//             console.log("este es el error", error);
+//         }
 
-    }
-    const enviarDatosServicioProducto = (event) =>{
-        console.log("estos son los datos", datos) 
-        Send();
-        console.log("este es el send", Send) 
-        // event.preventDefault()
-        // event.target.reset();
-    }
-    return{
-        handleChange,
-        enviarDatosServicioProducto
-    }
+//     }
+//     const enviarDatosServicioSP = (event) =>{
+//         SendSP();
+//         // event.preventDefault()
+//         // event.target.reset();
+//     }
+//     return{
+//         handleChange,
+//         enviarDatosServicioProducto
+//     }
 
-};
+// };
