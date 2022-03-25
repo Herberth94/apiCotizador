f5828dce6bbf4827786de27a4f27358afd32e649
CarlosDS369 <carlos@delfos369.com>
2022-03-24 22:46:40 -0600

    Se movió el buscador de proyectos
    El buscador ya se encuentra en el archivo de MargenGanancia
    
    En el archivo de operacionesAM ya se obtienen los objetos con los datos de los totales de las partidas y categorías de un proyectos

--- src/Administrator/AM/OperacionesAM.js
+++ src/Administrator/AM/OperacionesAM.js
@@ -8,14 +8,20 @@ import PTN_BOM from "../PTN-BOM/PTN_BOM";
 
 export const GetDatosProyecto = () => {
   /*=============================== Función que consulta los datos de un proyeco para el resumen AM ===============================*/
-    // Almacenamiento de datos
+    // Almacenamiento de los totales de las partidas
     const [totalesP,setTotalesP] = useState([]);
+
+    // Almacenamiento de los totales de las categorías
+    const [totalesC,setTotalesC] = useState([]);
 
     async function consultarTotalesP(id){
         //console.log(id)
         try{
             const resProy = await axios.get(url2 + `/api/cotizador/am/viewAM/${id}`);
             setTotalesP(resProy.data.data);
+
+            const resProyCats = await axios.get(url2 + `/api/cotizador/catt/view/${id}`);
+            setTotalesC(resProyCats.data.data);
             
         }catch (error){
             console.log(error);
@@ -24,6 +30,7 @@ export const GetDatosProyecto = () => {
     }
     /*===============================================================================================================================*/
     console.log(totalesP);
+    console.log(totalesC);
   return{
     consultarTotalesP
   }
--- src/Administrator/AM/ResumenAM.js
+++ src/Administrator/AM/ResumenAM.js
@@ -7,138 +7,13 @@ import { url, url2 } from "../../Componentes/Ocultar";
 import {partidasUnicas, descripcionGeneral, totalMXN, totalUSD, monedaPTN2} from "./OperacionesAM";
 
 function ResumenAM() {
-    //Habilitar/Deshabilitar tabla del resumen AM
-    const [show, setShow] = useState(true)
-
-    /*======================================== Buscador de proyectos ========================================*/
-    /*== Almacenamiento de todos los proyectos existentes ==*/
-    const[listaProyectos, setListaProyectos] = useState([]);
-
-    /*== Almacenamiento de los proyectos que tienen la clave semejante a la instroducida ==*/
-    const[suggestions,setSuggestions] = useState([]);
     
-    /*== Almacenamiento de la clave introducida del proyecto ==*/
-    const[claveP,setClaveP] = useState([]);
-
-    /*== Función que realiza la consulta a la tabla proyectos ==*/
-    useEffect(()=>{
-        const getProyectos = async () => {
-            try{
-                const resProy = await axios.get(url + '/api/cotizador/proyecto/view1');
-                setListaProyectos(resProy.data.data);
-            }catch(error){
-                console.log(error);
-            }
-        }
-        getProyectos();
-    },[])
-    
-    /*== Función que realiza la busqueda de los proyectos semejantes a la clave introducida ==*/
-    const onChangeTextClaveP = (claveP) => {
-        let coincidencias = [];
-        if(claveP.length>0){
-            coincidencias = listaProyectos.filter(proyecto => {
-            const regex = new RegExp(`${claveP}`, "gi");
-            return proyecto.proyecto_clave.match(regex)
-            })
-        }
-        setSuggestions(coincidencias);
-        setClaveP(claveP);
-    }
-    /*=======================================================================================================*/
-
-    
-    /*=============================== Función que consulta los datos de un proyeco para el resumen AM ===============================*/
-    // Almacenamiento de datos
-    const [totalesP,setTotalesP] = useState([]);
-
-    async function consultarTotalesP(id){
-        //console.log(id)
-        try{
-            const resProy = await axios.get(url2 + `/api/cotizador/am/viewAM/${id}`);
-            setTotalesP(resProy.data.data);
-            
-        }catch (error){
-            console.log(error);
-        }
-        console.log(totalesP);
-    }
-    /*===============================================================================================================================*/
     return (
         <div className="contenido-usuarios">
             <div>
                 {" "}
                 <Animaciones mytext="Resumen AM" />{" "}
             </div>
-
-            <div className="busqueda-proyectos">
-                    <Table responsive id="nombreDiv">
-                        <thead>
-                            <tr className="azul">
-                                <th>Clave Proyecto</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr >
-                                <td>
-                                <input className="agregar"
-                                        type="text"
-                                        name="proyecto_clave"
-                                        onChange={e => onChangeTextClaveP(e.target.value)}
-                                        value={claveP}
-                                        placeholder="Ingresa la clave del Proyecto" />
-                                </td>
-                            </tr>
-                        </tbody>
-                    </Table>
-                    {/****************************Lista de los Proyectos Creados ****************************************/}
-                {/*============= Titulo Animación =============*/}
-                <div className="container">
-                    <div className="box">
-                        <div className="title">
-                            <span className="block"></span>
-                            <h1 >Lista de Proyectos<span></span></h1>
-                        </div>
-                    </div>
-                </div>
-
-                <Table responsive  striped bordered hover size="sm">
-                    <thead>
-                        <tr className="titulo-tabla-usuarios">
-                            <th>ID</th>
-                            <th>Clave</th>
-                            <th>Descripción</th>
-                            <th>Cliente</th>
-                            <th>Fecha de creción</th>
-                            <th>Estatus</th>
-                            <th>Resumen AM</th>
-                        </tr>
-                    </thead>
-                                       
-                    <tbody>
-                        {Object.keys(suggestions).map((key) => (    
-                            //checar aqui va los titulos
-                            <tr key={suggestions[key].proyecto_id} >
-                                <td>{suggestions[key].proyecto_id}</td>   
-                                <td>{suggestions[key].proyecto_clave}</td>  
-                                <td>{suggestions[key].proyecto_descripcion}</td>  
-                                <td>{suggestions[key].nombre_cliente}</td> 
-                                <td>{suggestions[key].proyecto_fecha_creacion}</td>
-                                <td>{suggestions[key].proyecto_estatus}</td> 
-                                <td>
-                                    <button 
-                                    className="btn btn-primary" 
-                                    onClick={() => {consultarTotalesP(suggestions[key].proyecto_id);setShow(!show)}}
-                                    >{show ? 'Ver mas':'Ocultar proyecto'}</button>
-                                </td> 
-                            </tr>  
-                        ))}
-                    </tbody>          
-                </Table>
-            </div>
-            {show ? (
-                <div></div>
-            ):(
                 <div className="arregla">
                     <div className="contenido-usuarios">
                     <Table responsive striped bordered hover size="sm" className="tablas">
@@ -176,7 +51,6 @@ function ResumenAM() {
                     </Table>
                 </div>
             </div>
-            )}
         </div>
     );
 }
