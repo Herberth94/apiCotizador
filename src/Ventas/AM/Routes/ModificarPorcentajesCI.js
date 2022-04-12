ba074f5b169a6e41c231f8f1a216b3cec767b069
AndradeMalaika <malaikaandrade20@gmail.com>
2022-04-12 13:00:57 -0500

    agregando los try y catch en la parte de editar de los porcentajes partidas y categorias de  am

--- src/Ventas/AM/Routes/ModificarPorcentajesCI.js
+++ src/Ventas/AM/Routes/ModificarPorcentajesCI.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
-import {url2} from "../../../Componentes/Ocultar";
+import { url2 } from "../../../Componentes/Ocultar";
 
 export const EditCI = () => {
 
-    const actualizacion = (data,catCI,newdata)=>{
+    const actualizacion = (data, catCI, newdata) => {
         let ciId = '';
         let oldDataCI = '';
         let ciD = Object.keys(data);
         ciD = ciD.length;
-        for(let c = 0 ; c < ciD ; c++){
-            if(catCI === data[c].cci_nombre){
+        for (let c = 0; c < ciD; c++) {
+            if (catCI === data[c].cci_nombre) {
                 ciId = data[c].ci_id;
                 oldDataCI = data[c].ci_porcentaje;
                 //console.log(amId);
@@ -21,36 +21,38 @@ export const EditCI = () => {
         // console.log('Old data:',oldDataCI);
         // console.log('New data:',newdata);
         //console.log(dataCats);
-        Send(oldDataCI, ciId,newdata)      
+        Send(oldDataCI, ciId, newdata)
     }
 
-    async function Send (oldData,ciId,newdata){
+    async function Send(oldData, ciId, newdata) {
 
         const newData = {
-            ci_porcentaje:newdata.porcentaje
+            ci_porcentaje: newdata.porcentaje
         }
 
         const dataActualizacion = {
-            ci_porcentaje:oldData
+            ci_porcentaje: oldData
         };
 
         const k = Object.keys(newData);
-        if(newData[k] !== ''){
+        if (newData[k] !== '') {
             dataActualizacion[k] = newData[k];
         }
 
-        
-         
+
+
         try {
             //console.log('Nuevos datos CI:',dataActualizacion);
             //console.log('ci_id:',ciId);
-            await axios.post(url2 + `/api/cotizador/ci/edit/${ciId}`,dataActualizacion);
-            alert('Porcenaje editado exitosamente');
+            const respuesta = await axios.post(url2 + `/api/cotizador/ci/edit/${ciId}`, dataActualizacion);
+            const respuestaBack = respuesta.data.msg;
+            alert(respuestaBack);
             
-            } catch (error) {
+
+        } catch (error) {
             console.log(error);
-                alert('Error al editar el porcentaje');
-            }
+            alert('Error al editar el porcentaje');
+        }
     }
     return {
         actualizacion
@@ -58,4 +60,3 @@ export const EditCI = () => {
 };
 
 
-    
