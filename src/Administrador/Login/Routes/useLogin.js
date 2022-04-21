dbdcc574809bf3c1d4ae266cea462be851b18275
CarlosDS369 <carlos@delfos369.com>
2022-04-21 12:36:03 -0500

    Correcciones

--- src/Administrador/Login/Routes/useLogin.js
+++ src/Administrador/Login/Routes/useLogin.js
@@ -32,7 +32,7 @@ export const useLogin = () => {
         console.log(send2)
         alert(send2.msg)
         cookies.set('id_usuario', send2.id_usuario , {path:"/"});
-        cookies.set('rol', send2.rol, {path:"/"});
+        cookies.set('rol', send2.rol_nombre, {path:"/"});
         cookies.set('estado_login', send2.estado_login, {path:"/"});
         console.log(cookies.get('estado_login'));
         window.location.href="../";
