f123730a45d474a3da67b2ef2862a93f10c917d6
Cotizador <herberth@delfos369.com>
2022-05-09 21:30:10 +0200

    checar

--- src/Componentes/Ocultar.js
+++ src/Componentes/Ocultar.js
@@ -2,13 +2,12 @@
 
 export let passwordCaptcha = "6LduyMEeAAAAAE3Ys74WMzqQx8nlV2X1OP4NN_fn"
 
- export let url = 'http://localhost:4001';
+ /* export let url = 'http://localhost:4001';
 
 export let url2 = `http://localhost:4001`;
 
+ */
+ export let url = 'http://10.200.10.9:4001';
 
-/* export let url = 'http://10.200.10.9:4001';
+export let url2 = `http://10.200.10.9:4001`; 
 
-export let url2 = `http://10.200.10.9:4001`; */
-
-/* http://10.200.10.9:3000/ */
