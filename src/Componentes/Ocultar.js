4b052009626dfff5f04a01218d2180067b7f674a
oscar <oscar.delfos369.com>
2022-05-16 13:40:22 -0500

    url

--- src/Componentes/Ocultar.js
+++ src/Componentes/Ocultar.js
@@ -2,21 +2,15 @@
 
 export let passwordCaptcha = "6LduyMEeAAAAAE3Ys74WMzqQx8nlV2X1OP4NN_fn"
 
-<<<<<<< HEAD
-/*  export let url = 'http://localhost:4001';
-=======
 export let puerto = ':4001';
 export let puerto2= `:4001` ; 
->>>>>>> 171d387c74c192d5034494e9ab7b0a3b23009254
 
 
  export let url = 'http://localhost' + puerto;
 
 export let url2 = `http://localhost` + puerto2; 
 
- */
-export let url = 'http://10.200.10.9:4001';
-export let url2 = `http://10.200.10.9:4001`;  
+
 
 // export let url = 'http://200.194.42.18:4001';
 // export let url2 = `http://200.194.42.18:4001`;  
